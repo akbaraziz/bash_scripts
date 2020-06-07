8fe079b829621b2832bcb13644f5c84ba3d7c7f0
Akbar Aziz <akbar.aziz@nutanix.com>
2020-06-07 13:27:42 -0500

    Updated Scripts

---
+++ tested/shell_script_template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script author: Akbar Aziz
+# Script site: https://github.com/akbaraziz/bash_scripts
+# Script date: 06/05/2020
+# Script ver: 1.0
+# Script tested on OS: 
+# Script purpose: 
+
+#--------------------------------------------------
+
+# Defibe Varialbes
+
+
+
+set -ex
