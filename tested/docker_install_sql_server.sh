1f7d193a35707550a32f8ddacaf786e0361c3195
Akbar Aziz <akbaraziz@gmail.com>
2020-12-02 17:25:37 -0600

    Updated Scripts

---
+++ tested/docker_install_sql_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=<YourStrong!Passw0rd>' \
+   -p 1433:1433 --name sql2019 \
+   -v /users/randolph/mssql:/mssql \
+   -d mcr.microsoft.com/mssql/server:2019-latest
