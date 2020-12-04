ae325cf20686cc43c70a070b6dcdd90ed663b97a
Akbar Aziz <akbaraziz@gmail.com>
2020-12-03 22:28:22 -0600

    initial commit

---
+++ not-tested/install_artifactory_ce.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+
+set -ex  
+
+# Install Java
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
+
+# Set JAVA_HOME
+export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+source ~/.bashrc
+echo $JAVA_HOME
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+source ~/.bashrc
+
+# Download and Create Artifactory CE Repo
+wget https://bintray.com/jfrog/artifactory-rpms/rpm -O bintray-jfrog-artifactory-oss-rpms.repo;
+sudo mv  bintray-jfrog-artifactory-oss-rpms.repo /etc/yum.repos.d/;
+
+# Install Artifactory CE
+yum install jfrog-artifactory-oss -y
