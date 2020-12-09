84548a4c610530aa88d414e68095563119408d56
Akbar Aziz <akbaraziz@gmail.com>
2020-12-09 14:08:33 -0600

    Updated Scripts

--- tested/install_elastic_search.sh
+++ tested/install_elastic_search.sh
@@ -10,10 +10,10 @@
 
 set -ex
 
-#Import ElasticSearch PGP Key
+# Import ElasticSearch PGP Key
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-#Create RPM Repository
+# Create RPM Repository
 cat >/etc/yum.repos.d/elasticsearch.repo <<EOL 
 [elasticsearch]
 name=Elasticsearch repository for 7.x packages
