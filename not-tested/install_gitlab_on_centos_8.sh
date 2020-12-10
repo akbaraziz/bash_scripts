4ad1b0227eea6f0638a95b1185e01137fcc22a87
Akbar Aziz <akbaraziz@gmail.com>
2020-12-10 17:50:05 -0600

    Updated Scripts

--- not-tested/install_gitlab_on_centos_8.sh
+++ not-tested/install_gitlab_on_centos_8.sh
@@ -2,19 +2,26 @@
 
 set -ex
 
-EXTERNAL_URL="https://@@{HOST_NAME}@@"
+GITLAB_DNS_NAME="https://@@{HOST_NAME}@@"
 
 # Install Dependencies
-sudo dnf install -y curl policycoreutils openssh-server postfix
+sudo dnf install -y curl policycoreutils openssh-server postfix checkpolicy policycoreutils-python-utils python3-audit python3-libsemanage python3-policycoreutils python3-setools
 sudo systemctl enable --now sshd
 
 # Set Firewall Rules
-sudo firewall-cmd --permanent --add-service=http
-sudo firewall-cmd --permanent --add-service=https
-sudo systemctl reload firewall
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --permanent --zone=public --add-port=22/tcp
+sudo firewall-cmd --reload
 
 # Create GitLab Repo
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
 
 # Install GitLab
-sudo $EXTERNAL_URL dnf install -y gitlab-ce
+sudo dnf install -y gitlab-ce
+
+# Configure GitLab
+if [[ "${GITLAB_DNS_NAME}x" != "x" ]]; then 
+  sudo sed -i "s#external_url 'http://gitlab.example.com'#external_url 'http://${GITLAB_DNS_NAME}'#" /etc/gitlab/gitlab.rb
+fi
+sudo gitlab-ctl reconfigure
