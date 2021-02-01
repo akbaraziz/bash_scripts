ff87e0852a5c4b963afecc235b8e68ff86034a56
akbaraziz <akbar.aziz@nutanix.com>
2021-01-31 23:15:01 -0600

    Updated Scripts

--- tested/install_graylog.sh
+++ tested/install_graylog.sh
@@ -16,8 +16,13 @@ sudo sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"${HOST_NAME}"'/' /etc/sysconfig/netw
 sudo echo -e ""${VM_IP}" \t "${HOST_NAME}"" >> /etc/hosts
 
 
-sudo yum update -y
-sudo setenforce 0
+# Disable SELINUX
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+# Disable Firewall
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
 
 ## Install Java
 sudo yum -y install epel-release
@@ -31,9 +36,9 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' | sudo tee -a /etc/yum.repos.d/mongodb-org-4.2.repo
+sudo yum makecache
 sudo yum install -y mongodb-org
 sudo systemctl enable --now mongod
-sudo systemctl --type=service --state=active |grep mongod
 
 
 ## Install and configure elastic search
@@ -44,7 +49,7 @@ baseurl=https://artifacts.elastic.co/packages/oss-7.x/yum
 gpgcheck=1
 gpgkey=https://packages.elastic.co/GPG-KEY-elasticsearch
 enabled=1' | sudo tee -a /etc/yum.repos.d/elasticsearch.repo
-
+sudo yum makecache
 sudo yum -y install elasticsearch-oss
 
 sudo tee -a /etc/elasticsearch/elasticsearch.yml > /dev/null <<EOT
@@ -52,18 +57,19 @@ cluster.name: graylog
 action.auto_create_index: false
 EOT
 
+sudo systemctl daemon-reload
 sudo systemctl enable --now elasticsearch
-sudo systemctl --type=service --state=active | grep elasticsearch
 
 sleep 15
 
-## Verify elastic search
+## Verify elastic search 
 sudo curl -XGET 'http://localhost:9200/_cluster/health?pretty=true'
 
 
 ## Install graylog
 sudo rpm -Uvh https://packages.graylog2.org/repo/packages/graylog-4.0-repository_latest.rpm
-sudo yum update -y && sudo yum install -y graylog-server graylog-enterprise-plugins graylog-integrations-plugins graylog-enterprise-integrations-plugins
+sudo yum update -y
+sudo yum install -y graylog-server
 
 
 ## Configure graylog conf
@@ -83,18 +89,17 @@ elasticsearch_discovery_zen_ping_unicast_hosts = ${VM_IP}:9300
 elasticsearch_shards=1
 script.inline: false
 script.indexed: false
+http_bind_address = ${VM_IP}
 script.file: false" | sudo tee -a /etc/graylog/server/server.conf
 
 
 
 sudo sed -i '/web_listen_uri/s/^#//' /etc/graylog/server/server.conf
-
 sudo sed -i "/rest_listen_uri/s/127.0.0.1/${VM_IP}/" /etc/graylog/server/server.conf
 sudo sed -i "/web_listen_uri/s/127.0.0.1/${VM_IP}/" /etc/graylog/server/server.conf
 
 
 ## Start graylog service
+sudo systemctl daemon-reload
 sudo systemctl enable --now graylog-server
 sudo systemctl status graylog-server
-
-
